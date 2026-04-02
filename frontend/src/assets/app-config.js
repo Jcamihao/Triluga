c9448233cf28a845ae746806aa82f69e7ff6a297
João Marcos Cabral <johnmarcus3210@gmail.com>
2026-04-02 15:47:08 -0300

    chore: update frontend app config

--- frontend/src/assets/app-config.js
+++ frontend/src/assets/app-config.js
@@ -1,5 +1,5 @@
 window.__APP_CONFIG__ = {
-  "apiBaseUrl": "http://192.168.0.8:3000/api/v1",
-  "wsBaseUrl": "http://192.168.0.8:3000",
+  "apiBaseUrl": "http://192.168.0.4:3000/api/v1",
+  "wsBaseUrl": "http://192.168.0.4:3000",
   "clientLoggingEnabled": true
 };
